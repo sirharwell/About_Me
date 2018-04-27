09403e4443decbf1152a03db5fda4dcee9569248
Ian Harwell <sirharwell@gmail.com>
2018-04-26 23:44:59 -0600

    remove materialize

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import {Parallax} from 'react-materialize';
-import $ from 'jquery';
+import {Button, Icon, Parallax} from 'react-materialize';
 
 
 
 class App extends Component {
 
-  componentDidMount() {
-    $('.parallax').parallax();
-  }
 
   render() {
     return (
       <div>
-  <Parallax imageSrc="http://materializecss.com/images/parallax1.jpg"/>
-  <div className="section white">
-    <div className="row container">
-      <h2 className="header">Parallax</h2>
-      <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+        <div>
+    <Parallax imageSrc="http://materializecss.com/images/parallax1.jpg"/>
+    <div className="section white">
+      <div className="row container">
+        <h2 className="header">Parallax</h2>
+        <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+      </div>
     </div>
+    <Parallax imageSrc="http://materializecss.com/images/parallax2.jpg"/>
   </div>
-  <Parallax imageSrc="http://materializecss.com/images/parallax2.jpg"/>
-</div>
-
+          </div>
     );
   }
 }
