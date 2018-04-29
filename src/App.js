9634b53548223aa7478fabcb8b1976299b15f34f
Ian Harwell <sirharwell@gmail.com>
2018-04-28 22:15:21 -0600

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
 import {
   Button,
   Container,
@@ -10,271 +9,154 @@ import {
   Image,
   List,
   Menu,
-  Responsive,
   Segment,
   Sidebar,
-  Visibility,
 } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 
-/* eslint-disable react/no-multi-comp */
-/* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
- * such things.
- */
-const HomepageHeading = ({ mobile }) => (
-  <Container text>
-    <Header
-      as='h1'
-      content='Imagine-a-Company'
-      inverted
-      style={{
-        fontSize: mobile ? '2em' : '4em',
-        fontWeight: 'normal',
-        marginBottom: 0,
-        marginTop: mobile ? '1.5em' : '3em',
-      }}
-    />
-    <Header
-      as='h2'
-      content='Do whatever you want when you want to.'
-      inverted
-      style={{
-        fontSize: mobile ? '1.5em' : '1.7em',
-        fontWeight: 'normal',
-        marginTop: mobile ? '0.5em' : '1.5em',
-      }}
-    />
-    <Button primary size='huge'>
-      Get Started
-      <Icon name='right arrow' />
-    </Button>
-  </Container>
-)
-
-HomepageHeading.propTypes = {
-  mobile: PropTypes.bool,
-}
-
-/* Heads up!
- * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
- * It can be more complicated, but you can create really flexible markup.
- */
-class DesktopContainer extends Component {
-  state = {}
-
-  hideFixedMenu = () => this.setState({ fixed: false })
-  showFixedMenu = () => this.setState({ fixed: true })
-
-  render() {
-    const { children } = this.props
-    const { fixed } = this.state
-
-    return (
-      <Responsive {...Responsive.onlyComputer}>
-        <Visibility once={false} onBottomPassed={this.showFixedMenu} onBottomPassedReverse={this.hideFixedMenu}>
-          <Segment inverted textAlign='center' style={{ minHeight: 700, padding: '1em 0em' }} vertical>
-            <Menu
-              fixed={fixed ? 'top' : null}
-              inverted={!fixed}
-              pointing={!fixed}
-              secondary={!fixed}
-              size='large'
-            >
-              <Container>
-                <Menu.Item as='a' active>Home</Menu.Item>
-                <Menu.Item as='a'>Work</Menu.Item>
-                <Menu.Item as='a'>Company</Menu.Item>
-                <Menu.Item as='a'>Careers</Menu.Item>
-                <Menu.Item position='right'>
-                  <Button as='a' inverted={!fixed}>Log in</Button>
-                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>Sign Up</Button>
-                </Menu.Item>
-              </Container>
-            </Menu>
-            <HomepageHeading />
+class Home extends React.Component {
+  render(){
+    return(
+      <div>
+      <Segment inverted textAlign='center' style={{
+        backgroundImage: `url(https://images.pexels.com/photos/239886/pexels-photo-239886.jpeg?cs=srgb&dl=architectural-design-architecture-blueprint-239886.jpg&fm=jpg)`, backgroundSize: " cover ", minHeight: 700, padding: '1em 0em' }} vertical
+      >
+        <Container text style={{marginTop:'170px'}}>
+          <Segment style={{ backgroundColor: 'rgba(250, 250, 250, .8)', padding: '50px'}}>
+            <Image src='https://image.ibb.co/dZuAmn/canduu_combined.png'></Image>
+            <Link to='./'>
+            <Button color='blue' size='huge'>
+              I Need...
+              <Icon name='right arrow' />
+            </Button>
+            </Link>
           </Segment>
-        </Visibility>
-
-        {children}
-      </Responsive>
-    )
-  }
-}
-
-DesktopContainer.propTypes = {
-  children: PropTypes.node,
-}
-
-class MobileContainer extends Component {
-  state = {}
-
-  handlePusherClick = () => {
-    const { sidebarOpened } = this.state
-
-    if (sidebarOpened) this.setState({ sidebarOpened: false })
-  }
-
-  handleToggle = () => this.setState({ sidebarOpened: !this.state.sidebarOpened })
-
-  render() {
-    const { children } = this.props
-    const { sidebarOpened } = this.state
-
-    return (
-      <Responsive {...Responsive.onlyMobile}>
-        <Sidebar.Pushable>
-          <Sidebar as={Menu} animation='uncover' inverted vertical visible={sidebarOpened}>
-            <Menu.Item as='a' active>Home</Menu.Item>
-            <Menu.Item as='a'>Work</Menu.Item>
-            <Menu.Item as='a'>Company</Menu.Item>
-            <Menu.Item as='a'>Careers</Menu.Item>
-            <Menu.Item as='a'>Log in</Menu.Item>
-            <Menu.Item as='a'>Sign Up</Menu.Item>
-          </Sidebar>
-
-          <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh' }}>
-            <Segment inverted textAlign='center' style={{ minHeight: 350, padding: '1em 0em' }} vertical>
-              <Container>
-                <Menu inverted pointing secondary size='large'>
-                  <Menu.Item onClick={this.handleToggle}>
-                    <Icon name='sidebar' />
-                  </Menu.Item>
-                  <Menu.Item position='right'>
-                    <Button as='a' inverted>Log in</Button>
-                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>
-                  </Menu.Item>
-                </Menu>
-              </Container>
-              <HomepageHeading mobile />
-            </Segment>
-
-            {children}
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </Responsive>
-    )
-  }
-}
-
-MobileContainer.propTypes = {
-  children: PropTypes.node,
-}
-
-const ResponsiveContainer = ({ children }) => (
-  <div>
-    <DesktopContainer>{children}</DesktopContainer>
-    <MobileContainer>{children}</MobileContainer>
-  </div>
-)
-
-ResponsiveContainer.propTypes = {
-  children: PropTypes.node,
-}
-
-const HomepageLayout = () => (
-  <ResponsiveContainer>
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Grid container stackable verticalAlign='middle'>
-        <Grid.Row>
-          <Grid.Column width={8}>
-            <Header as='h3' style={{ fontSize: '2em' }}>We Help Companies and Companions</Header>
-            <p style={{ fontSize: '1.33em' }}>
-              We can give your company superpowers to do things that they never thought possible. Let us delight
-              your customers and empower your needs... through pure data analytics.
-            </p>
-            <Header as='h3' style={{ fontSize: '2em' }}>We Make Bananas That Can Dance</Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.
-            </p>
-          </Grid.Column>
-          <Grid.Column floated='right' width={6}>
-            <Image
-              bordered
-              rounded
-              size='large'
-              src='/assets/images/wireframe/white-image.png'
-            />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column textAlign='center'>
-            <Button size='huge'>Check Them Out</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-    <Segment style={{ padding: '0em' }} vertical>
-      <Grid celled='internally' columns='equal' stackable>
-        <Grid.Row textAlign='center'>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as='h3' style={{ fontSize: '2em' }}>"What a Company"</Header>
-            <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
-          </Grid.Column>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as='h3' style={{ fontSize: '2em' }}>"I shouldn't have gone with their competitor."</Header>
-            <p style={{ fontSize: '1.33em' }}>
-              <Image avatar src='/assets/images/avatar/large/nan.jpg' />
-              <b>Nan</b> Chief Fun Officer Acme Toys
-            </p>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Container text>
-        <Header as='h3' style={{ fontSize: '2em' }}>Breaking The Grid, Grabs Your Attention</Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Instead of focusing on content creation and hard work, we have learned how to master the art of doing
-          nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic
-          and worth your attention.
-        </p>
-        <Button as='a' size='large'>Read More</Button>
-        <Divider
-          as='h4'
-          className='header'
-          horizontal
-          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-        >
-          <a href='#'>Case Studies</a>
-        </Divider>
-        <Header as='h3' style={{ fontSize: '2em' }}>Did We Tell You About Our Bananas?</Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but it's really
-          true.
-          It took years of gene splicing and combinatory DNA research, but our bananas can really dance.
-        </p>
-        <Button as='a' size='large'>I'm Still Quite Interested</Button>
-      </Container>
-    </Segment>
-    <Segment inverted vertical style={{ padding: '5em 0em' }}>
-      <Container>
-        <Grid divided inverted stackable>
+        </Container>
+      </Segment>
+      <Segment>
+        <Container>
+          <Grid computer={6} tablet={3} mobile={1} divided centered>
+            <Grid.Row centered>
+              <Grid.Column style={{width: '125px'}}>
+              <Link to='/' centered style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <Icon className='home' centered verticalAlign='middle' size='big'/> <Header textAlign='center' as="h3">Home Repair</Header>
+              </Link>
+              </Grid.Column>
+              <Grid.Column style={{width: '125px'}}>
+              <Link to='/' centered style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <Icon className='heartbeat' size='big'/> <Header textAlign='center' as="h3">Health & Wellness</Header>
+              </Link>
+              </Grid.Column>
+              <Grid.Column style={{width: '125px'}}>
+              <Link to='/' centered style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <Icon className='camera retro' size='big'/> <Header textAlign='center' as="h3">Media</Header>
+              </Link>
+              </Grid.Column>
+              <Grid.Column style={{width: '125px'}}>
+              <Link to='/' centered style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <Icon className='book' size='big'/> <Header textAlign='center' as="h3">Lessons</Header>
+              </Link>
+              </Grid.Column>
+              <Grid.Column style={{width: '125px'}}>
+              <Link to='/' centered style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <Icon className='computer' size='big'/> <Header textAlign='center' as="h3">Websites & Design</Header>
+              </Link>
+              </Grid.Column>
+              <Grid.Column style={{width: '125px'}} >
+              <Link to='/' centered style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <Icon className='paint brush' textAlign='center' size='big'/> <Header textAlign='center' as="h3">Custom Art</Header>
+              </Link>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Segment>
+      <Segment style={{ padding: '8em 0em' }} vertical>
+        <Grid container stackable verticalAlign='middle'>
           <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='About' />
-              <List link inverted>
-                <List.Item as='a'>Sitemap</List.Item>
-                <List.Item as='a'>Contact Us</List.Item>
-                <List.Item as='a'>Religious Ceremonies</List.Item>
-                <List.Item as='a'>Gazebo Plans</List.Item>
-              </List>
+            <Grid.Column width={8}>
+              <Header as='h3' style={{ fontSize: '2em' }}>We Find Local Professionals You Can Trust!</Header>
+              <p style={{ fontSize: '1.33em' }}>
+                We get all of our professionals and run them through a rigerous process to weed out those who don't deserve your business. We only work with the highest quality and affordable professionals who do what they say they will do.
+              </p>
+              <Header as='h3' style={{ fontSize: '2em' }}>Your Contractor Doesn't Get Paid Until You Are 100% Satisfied!</Header>
+              <p style={{ fontSize: '1.33em' }}>
+                Once you find a contractor and agree to how and when your project will be finished, you fund the project and we hold the funds until you are 100% satisfied with the work done. If you aren't happy, we will refund you 100%.
+              </p>
             </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Services' />
-              <List link inverted>
-                <List.Item as='a'>Banana Pre-Order</List.Item>
-                <List.Item as='a'>DNA FAQ</List.Item>
-                <List.Item as='a'>How To Access</List.Item>
-                <List.Item as='a'>Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as='h4' inverted>Footer Header</Header>
-              <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+            <Grid.Column floated='right' width={6}>
+              <Image
+                bordered
+                rounded
+                size='large'
+                src='https://images.pexels.com/photos/259962/pexels-photo-259962.jpeg?cs=srgb&dl=apartment-architecture-ceiling-259962.jpg&fm=jpg'
+              />
             </Grid.Column>
           </Grid.Row>
         </Grid>
-      </Container>
-    </Segment>
-  </ResponsiveContainer>
-)
-export default HomepageLayout
+      </Segment>
+      <Segment style={{ padding: '0em' }} vertical>
+        <Grid celled='internally' columns='equal' stackable>
+          <Grid.Row textAlign='center'>
+            <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+              <Header as='h3' style={{ fontSize: '2em' }}>"I found many professionals who were eager to work on my project and all gave me competitive quotes. I love Can Duu!!!"</Header>
+              <p style={{ fontSize: '1.33em' }}>-@james_Jessie</p>
+            </Grid.Column>
+            <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+              <Header as='h3' style={{ fontSize: '2em' }}>"I never use anyone else to find someone for a project! Can Duu is the best!"</Header>
+              <p style={{ fontSize: '1.33em' }}> -@brad-f
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment style={{ padding: '8em 0em' }} vertical>
+        <Container text>
+          <Header as='h3' style={{ fontSize: '2em' }}>We Have All The Right People!</Header>
+          <p style={{ fontSize: '1.33em' }}>
+            When you need a handyman, photographer, plumber, landscaper, CPA, tailor, and just about anything else you can think of, we have your back!
+            <br/>
+            <br/>
+            We have all the people you need to get your projects done, and done right. We have partnered with many organizations to provide you with the highest qualified, local professionals to take care of your projects in a timely and cost-effective way. You won't pay a dime for a quote and you can rest assured that the project will be done right the first time or it's on us!
+          </p>
+          <Divider
+            as='h4'
+            className='header'
+            horizontal
+            style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+          >
+          </Divider>
+        </Container>
+      </Segment>
+      <Segment inverted vertical style={{ padding: '2em 0em' }}>
+        <Container>
+          <Grid divided inverted stackable>
+            <Grid.Row>
+              <Grid.Column width={5}>
+                <Header inverted as='h4' content='Contact Us' />
+                <div link inverted>
+                  <p>Phone: (801) 555-5555</p>
+                  <p>Email: info@canduu.com</p>
+                  <p>Address: 300 East 370 South<br /> Salt Lake City, Utah 84111</p>
+                  <div textAlign='center' relaxed columns={3}>
+                      <Button circular color='facebook' icon='facebook' />
+                      <Button circular color='twitter' icon='twitter' />
+                      <Button circular color='linkedin' icon='linkedin' />
+                      <Button circular color='google plus' icon='google plus' />
+                    </div>
+                </div>
+              </Grid.Column>
+              <Grid.Column width={10} verticalAlign="middle">
+                <Header as='h4' inverted>About</Header>
+                <p>We get all of our professionals and run them through a rigerous process to weed out those who do not deserve your business. We only work with the highest quality and affordable professionals who do what they say they will do.</p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Segment>
+      </div>
+    )
+  }
+}
+
+export default Home;
