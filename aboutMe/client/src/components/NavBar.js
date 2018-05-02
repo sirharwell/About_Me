156c4befd1e9e4561bfe7176b1652e63c8017e7a
Ian Harwell <sirharwell@gmail.com>
2018-05-01 20:07:21 -0600

    routing

--- aboutMe/client/src/components/NavBar.js
+++ aboutMe/client/src/components/NavBar.js
@@ -11,32 +11,6 @@ class NavBar extends Component {
     unread: this.props.unread,
   }
 
-  rightNavs = () => {
-    const { user, dispatch, history, activeItem, unread} = this.props;
-
-    if (user.id) {
-      return (
-          <Menu.Menu position='right'>
-            <Menu.Item as='a' href="/user_notifications" active={activeItem === '/user_notifications'} >Notifications <Label>
-              <Icon name='mail' /> {unread}
-            </Label></Menu.Item>
-            <Menu.Item as='a' href="/MyListings" active={activeItem === '/MyListings'} >My Listings</Menu.Item>
-            <Menu.Item as='a' href="/Profile" active={activeItem === '/Profile'} >Profile</Menu.Item>
-            <Menu.Item
-              name='Logout'
-              onClick={() => dispatch(handleLogout(history))}
-            />
-          </Menu.Menu>
-      );
-    }
-    return (
-        <Menu.Item position='right'>
-          <Button as='a'  href="/login">Log in</Button>
-          <Button as='a' color="blue" href="/register" style={{ marginLeft: '0.5em' }}>Sign Up</Button>
-        </Menu.Item>
-
-    );
-  }
 
   render() {
     const { activeItem } = this.state
@@ -47,14 +21,10 @@ class NavBar extends Component {
         size='large'
         pointing
       >
-        <Link to='/'>
-          <Menu.Item>
-            <Image size='mini' src='https://image.ibb.co/cMmewc/simple_logo.png' />
-          </Menu.Item>
         </Link>
         <Menu.Item as='a' href="/" active={activeItem === '/'} >Home</Menu.Item>
-        <Menu.Item as='a' href="/CreateJob" active={activeItem === '/CreateJob'}>I need...</Menu.Item>
-        <Menu.Item as='a' href="/jobs" active={activeItem === '/jobs'}>Jobs</Menu.Item>
+        <Menu.Item as='a' href="/" active={activeItem === '/'}>I need...</Menu.Item>
+        <Menu.Item as='a' href="/" active={activeItem === '/'}>Jobs</Menu.Item>
           { this.rightNavs() }
         </Menu>
     )
