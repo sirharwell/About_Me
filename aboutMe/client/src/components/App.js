6d3f42ace1a11a8c611a96d6dfe7a52aa889ef0c
Ian Harwell <sirharwell@gmail.com>
2018-05-17 13:05:45 -0600

    routes

--- aboutMe/client/src/components/App.js
+++ aboutMe/client/src/components/App.js
@@ -5,6 +5,7 @@ import Login from './Login';
 import Register from './Register';
 import Flash from './Flash';
 import Home from './Home';
+import WorkXP from './WorkXP'
 import ProtectedRoute from './ProtectedRoute';
 import AuthRoute from './AuthRoute';
 import FetchUser from './FetchUser';
