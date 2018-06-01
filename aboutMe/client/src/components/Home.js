9e7cab1da0790c5b5e7e0f3d918e248533eb8b8b
Ian Harwell <sirharwell@gmail.com>
2018-05-31 21:18:31 -0600

    link

--- aboutMe/client/src/components/Home.js
+++ aboutMe/client/src/components/Home.js
@@ -38,7 +38,7 @@ class Home extends React.Component {
               <Header as='h3' style={{ fontSize: '2em' }}>I have been married for 8 years</Header>
               <p style={{ fontSize: '1.33em' }}>
                 She's the super coolest, you can learn more about her
-              </p><a href=''>here.</a>
+              </p><a href='/family'>here.</a>
             </Grid.Column>
             <Grid.Column floated='right' width={6}>
               <Image
