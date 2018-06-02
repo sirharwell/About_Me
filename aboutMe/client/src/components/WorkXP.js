f28b95c2a344b4840f004bd880476246af1e24bb
Ian Harwell <sirharwell@gmail.com>
2018-06-02 11:24:34 -0600

    canduu

--- aboutMe/client/src/components/WorkXP.js
+++ aboutMe/client/src/components/WorkXP.js
@@ -19,7 +19,7 @@ class Work extends React.Component {
     return(
       <div>
       <Segment inverted textAlign='center' style={{
-        backgroundImage: `url(https://lh3.googleusercontent.com/xozwtcTAexOfoLmqvGjLmwt141ZfwsAGKJDAldLnfHOnc0msmNsjcoK7-sGuEAELkDoY8clHtNynwv-OkB6v8JJFzVS1BDfrhioGFZtCwj-Worcd65uvucxXDeJ0fwE6PXMgkz9N_Ueq3QjEYzLDaZLtCybvLDxAMlJH7ViQNnqwOfGt_98xfC0o44a_ABzyeHEx5sOexgsjiK91mpYEPWesdE1jzkkdZN2FTNuPnBY7pUWpUPovJOdt2x5fATY__oNRyf3HXIe3tsgMwQCzu6oQuidyj5vwzG8ug88V9h5IjqsRfS3YmGX51ncBIQooicZMfwPe6V8hqnkXG3uNy8pqzDgoT7T5Y4uehPGPajJ3ZNpNmaukwQ5hmSUtKXLv8iHSVyMDGBI76MEmCqgr9PrMFosakl8T8N2KfneCmMH5pkgxxUOVNNjDfl7V2KxyvCnsixM7XBcUIZQ1J4HVN8uti-5U6PNvZ9Tb3q9lDo2XrdGzibm5zX6RjiupTn28uNaENOPerTzEakmZSlNP7fMxmxN7U5rGZeF3r66wy9Mv3sa7kAVcI9r1cvwX1qi9cdl9tVF47yuJLLug9QS_Wzx6YzkMeSpHCzC1F944=w1012-h759-no)`, backgroundSize: " cover ", minHeight: 700, padding: '1em 0em' }} vertical
+        backgroundImage: `url(./photos/canduu.jpg)`, backgroundSize: " cover ", minHeight: 700, padding: '1em 0em' }} vertical
       >
         <Container text style={{marginTop:'30px'}}>
           <Segment style={{ backgroundColor: 'rgba(250, 250, 250, .0)', padding: '20px'}}>
